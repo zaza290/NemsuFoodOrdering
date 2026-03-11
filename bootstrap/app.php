253124825d4a460e3d8f325576d4181a69cada25
Wilmer <wgsanchez@nemsu.edu.ph>
2026-03-11 20:08:48 +0800

    Fix: Reverb WSS and TrustProxies for Render deployment

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,11 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-    $middleware->alias([
-        'admin' => \App\Http\Middleware\AdminMiddleware::class,
-    ]);
-})
+        $middleware->trustProxies(at: '*');
+        $middleware->alias([
+            'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        ]);
+    })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
